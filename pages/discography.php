987ca4adcc8c5b3b68eb7411076f71c3dde49b66
Jonatan Ruben <medinaarcos.jonatanruben@gmail.com>
2023-07-13 07:13:39 -0500

    Gorillaz V2 13/07/2023: Discography Page Finished

--- pages/discography.php
+++ pages/discography.php
@@ -27,11 +27,11 @@ require('../layout/header-nav.html');
         <h2 class="section__h2 section__h2--space-bar">Discography</h2>
     </section>
     
-    <!--  -->
+    <!-- Sub-Banner 1: Studio Albums -->
     <section class="section-full section-full--discography">
         <section class="sub-section sub-section--discography-img">
         </section>
-        <section class="sub-section sub-section--discography-text">
+        <section class="sub-section sub-section--discography-text sub-section--discography-text--right">
             <h2 class="sub-section__h2 sub-section__h2--discography">Studio Albums</h2>
             <article class="sub-section__article">
                 <ul class="sub-section__ul">
@@ -50,7 +50,7 @@ require('../layout/header-nav.html');
     <!-- Section: Album 1 -->
     <section class="section-full section-full__album section-full__album--1">
         <section class="sub-section sub-section__album-img">
-            <img src="../img/discography/gorillaz_album.jpg" alt="Gorillaz (Album)" class="sub-section__img sub-section__img--album-img" width="100%" height="100%">
+            <img src="../img/discography/gorillaz_album.jpg" loading="lazy" alt="Gorillaz (Album)" class="sub-section__img sub-section__img--album-img" width="100%" height="100%">
         </section>
         <section class="sub-section sub-section__album-text">
             <h2 class="sub-section__h2 sub-section__h2--album-text">Gorillaz (Album)</h2>
@@ -64,10 +64,10 @@ require('../layout/header-nav.html');
             </article>
         </section>
     </section>
-     <!-- Section: Album 1 -->
-     <section class="section-full section-full__album section-full__album--2">
+    <!-- Section: Album 2 -->
+    <section class="section-full section-full__album section-full__album--2">
         <section class="sub-section sub-section__album-img">
-            <img src="../img/discography/demon_album.jpg" alt="Demon Days" class="sub-section__img sub-section__img--album-img" width="100%" height="100%">
+            <img src="../img/discography/demon_album.jpg" loading="lazy" alt="Demon Days" class="sub-section__img sub-section__img--album-img" width="100%" height="100%">
         </section>
         <section class="sub-section sub-section__album-text">
             <h2 class="sub-section__h2 sub-section__h2--album-text sub-section__h2--album-text-white">Demon Days</h2>
@@ -77,6 +77,157 @@ require('../layout/header-nav.html');
                 It was released on 11 May 2005 in the United Kingdom by Parlophone and on 23 May 2005 in the United Stated by Virgin Records. 
                 Produced by the band, Danger Mouse, Jason Cox, and James Dring, the album features contributions from De La Soul, Neneh Cherry, 
                 Martina Topley-Bird, Roots Manuva, MF DOOM, Ike Turner, Bootle Brown, Shaun Ryder, and Dennis Hopper.
+                </p>
+            </article>
+        </section>
+    </section>
+    <!-- Section: Album 3 -->
+    <section class="section-full section-full__album section-full__album--3">
+        <section class="sub-section sub-section__album-img">
+            <img src="../img/discography/plastic-beach_album.jpg" loading="lazy" alt="Plastic Beach" class="sub-section__img sub-section__img--album-img" width="100%" height="100%">
+        </section>
+        <section class="sub-section sub-section__album-text">
+            <h2 class="sub-section__h2 sub-section__h2--album-text sub-section__h2--album-text-white">Plastic Beach</h2>
+            <article class="sub-section__article">
+                <p>
+                Plastic Beach is the third studio album by English virtual band Gorillaz. 
+                It was released on 3 March 2010 in the United Kingdom by Parlophone and in the United States by Virgin Records. 
+                Conceived from an unfinished project titled Carousel, the album was recorded from June 2008 to November 2009 and was produced primarily by group co-creator Damon Albarn.
+                </p>
+            </article>
+        </section>
+    </section>
+    <!-- Section: Album 4 -->
+    <section class="section-full section-full__album section-full__album--4">
+        <section class="sub-section sub-section__album-img">
+            <img src="../img/discography/the-fall_album.jpg" loading="lazy" alt="The Fall" class="sub-section__img sub-section__img--album-img" width="100%" height="100%">
+        </section>
+        <section class="sub-section sub-section__album-text">
+            <h2 class="sub-section__h2 sub-section__h2--album-text sub-section__h2--album-text-white">The Fall</h2>
+            <article class="sub-section__article">
+                <p>
+                The Fall is the fourth studio album by English virtual band Gorillaz. 
+                It was released on 25 December 2010 on the band's website and physically on 18 April in the United Kingdom by Parlophone and in the United States by Virgin Records. 
+                The entirety of the album was recorded and produced with group co-founder Damon Albarn's iPad during the North American leg of the Escape to Plastic Beach World Tour, from October to November 2010.
+                </p>
+            </article>
+        </section>
+    </section>
+    <!-- Section: Album 5 -->
+    <section class="section-full section-full__album section-full__album--5">
+        <section class="sub-section sub-section__album-img">
+            <img src="../img/discography/humanz_album.jpg" loading="lazy" alt="Humanz" class="sub-section__img sub-section__img--album-img" width="100%" height="100%">
+        </section>
+        <section class="sub-section sub-section__album-text">
+            <h2 class="sub-section__h2 sub-section__h2--album-text sub-section__h2--album-text-white">Humanz</h2>
+            <article class="sub-section__article">
+                <p>
+                Humanz is the fifth studio album by English virtual band Gorillaz. 
+                It was released on 28 April 2017 in the United Kingdom by Parlophone and in the United States by Warner Bros Records. 
+                Produced by the band, The Twilite Tone, and Remi Kabaka Jr.
+                , it was Gorillaz' first studio album in seven years, after The Fall (2010).
+                </p>
+            </article>
+        </section>
+    </section>
+    <!-- Section: Album 6 -->
+    <section class="section-full section-full__album section-full__album--6">
+        <section class="sub-section sub-section__album-img">
+            <img src="../img/discography/now_album.jpg" loading="lazy" alt="The Now Now" class="sub-section__img sub-section__img--album-img" width="100%" height="100%">
+        </section>
+        <section class="sub-section sub-section__album-text">
+            <h2 class="sub-section__h2 sub-section__h2--album-text">The Now Now</h2>
+            <article class="sub-section__article">
+                <p>
+                The Now Now is the sixth studio album by English virtual band Gorillaz. 
+                It was released on 29 June 2018 in the United Kingdom by Parlophone and in the United States by Warner Bros Records. 
+                Recording began with fewer collaborators than usual, due to criticism over the band's previous album Humanz (2017) having too many, 
+                during the North American leg of the Humanz Tour in late 2017.
+                </p>
+            </article>
+        </section>
+    </section>
+    <!-- Section: Album 7 -->
+    <section class="section-full section-full__album section-full__album--7">
+        <section class="sub-section sub-section__album-img">
+            <img src="../img/discography/song-machine_album.jpg" loading="lazy" alt="Song Machine: Season One" class="sub-section__img sub-section__img--album-img" width="100%" height="100%">
+        </section>
+        <section class="sub-section sub-section__album-text">
+            <h2 class="sub-section__h2 sub-section__h2--album-text">Song Machine: Season One</h2>
+            <article class="sub-section__article">
+                <p>
+                Song Machine: Season One is the upcoming seventh studio album by English virtual band Gorillaz, set to be released on 23 October 2020. 
+                The album is released as part of the Gorillaz Song Machine project, a collection of singles and music videos, dubbed "episodes" each featuring previously unannounced guests, 
+                and has incorporated a wide range of genres, thus far including punk rock, bossa nova, synth-pop, electronic, and downtempo.
+                </p>
+            </article>
+        </section>
+    </section>
+
+    <!-- Sub-Banner 2: Compilation Albums -->
+    <section class="section-full section-full--discography section-full--discography-2">
+        <section class="sub-section sub-section--discography-text sub-section--discography-text--left">
+            <h2 class="sub-section__h2 sub-section__h2--discography">Compilation Albums</h2>
+            <article class="sub-section__article">
+                <ul class="sub-section__ul">
+                    <li class="sub-section__li"><b>G-Sides</b> (2002)</li>
+                    <li class="sub-section__li"><b>D-Sides</b> (2007)</li>
+                    <li class="sub-section__li"><b>The Singles Collection</b> (2001-2011)</li>
+                    <li class="sub-section__li"><b>Almanac CD</b> (2020)</li>
+                </ul>
+            </article>
+        </section>
+        <section class="sub-section sub-section--discography-img-2">
+        </section>
+    </section>
+    <!-- Section: Album 8 -->
+    <section class="section-full section-full__album section-full__album--8">
+        <section class="sub-section sub-section__album-img">
+            <img src="../img/discography/g-sides_album.jpg" loading="lazy" alt="G-Sides" class="sub-section__img sub-section__img--album-img" width="100%" height="100%">
+        </section>
+        <section class="sub-section sub-section__album-text">
+            <h2 class="sub-section__h2 sub-section__h2--album-text">G-Sides</h2>
+            <article class="sub-section__article">
+                <p>
+                    G-Sides is a collection of tracks that previously appeared on the first four Gorillaz UK single releases (the Tomorrow Comes Today EP, "Clint Eastwood", "19-2000" and "Rock the House").
+                </p>
+                <br>
+                <p>
+                    The album was originally scheduled only for the Japanese market. In Japan short commercially released singles of 3-4 tracks are not as popular as they are in the UK / Australia, and 
+                    bands frequently released what are in effect extended EPs of tracks collected from several releases.
+                </p>
+            </article>
+        </section>
+    </section>
+    <!-- Section: Album 9 -->
+    <section class="section-full section-full__album section-full__album--9">
+        <section class="sub-section sub-section__album-img">
+            <img src="../img/discography/d-sides_album.jpg" loading="lazy" alt="D-Sides" class="sub-section__img sub-section__img--album-img" width="100%" height="100%">
+        </section>
+        <section class="sub-section sub-section__album-text">
+            <h2 class="sub-section__h2 sub-section__h2--album-text sub-section__h2--album-text-white">D-Sides</h2>
+            <article class="sub-section__article">
+                <p>
+                    D-Sides is a 2007 compilation album by Gorillaz. The album contains B-sides and remixes from singles and bonus tracks for the band's second studio album Demon Days.
+                    It was released on 19 November 2007 in the UK and on 20 November in the United States and is available in standard and deluxe editions.
+                    The standard Japanese edition features two extra tracks while the Japanese deluxe version has no extra tracks.
+                    The creature on the album cover is Pazuzu, a creature originally from the 1973 American supernatural horror film, The Exorcist.
+                </p>
+            </article>
+        </section>
+    </section>
+    <!-- Section: Album 10 -->
+    <section class="section-full section-full__album section-full__album--10">
+        <section class="sub-section sub-section__album-img">
+            <img src="../img/discography/single-collection_album.jpg" loading="lazy" alt="The Singles Collection 2001-201" class="sub-section__img sub-section__img--album-img" width="100%" height="100%">
+        </section>
+        <section class="sub-section sub-section__album-text">
+            <h2 class="sub-section__h2 sub-section__h2--album-text sub-section__h2--album-text-white">The Singles Collection 2001-201</h2>
+            <article class="sub-section__article">
+                <p>
+                    The Singles Collection: 2001-2011 is a compilation album featuring top tracks and singles.
+                    The album has four different editions: standard, deluxe, 12"vinyland 7" single box set.
+                    The title featuring the years 2001-2011 isn't entirely correct, seeing as "Tomorrow Comes Today" was first released in 2000, and "Doncamatic", the newest featuring track, was released in 2010.
                 </p>
             </article>
         </section>
